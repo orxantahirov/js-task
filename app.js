5c81df5f1dc3e63fd44ad9417636b0b0acad0d2f
tahirovorxan <orxantahirov310@gmail.com>
2024-04-29 23:28:40 -0700

    add all codes

---
+++ app.js
@@ -0,0 +1,16 @@
+// let a = prompt("haralisan qaqa");
+
+// if (a == "Baki") {
+//   console.log("catdirilma var");
+// } else {
+//   console.log("catdirilma yoxdur");
+// }
+
+// let button = document.getElementById("btn");
+// let modal = document.getElementById("modal");
+
+// button.addEventListener("click", () => {
+//   modal.classList.toggle("show");
+// });
+
+
