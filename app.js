12121098ec6564feac2431a7fd599a9057805a7c
tahirovorxan <orxantahirov310@gmail.com>
2024-05-01 00:18:13 -0700

    add

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// let a = prompt("bu eraziye catdirilma varmi?");
-// let b = "Baki";
-// if (a.toLowerCase() == b.toLowerCase()) {
-//   console.log("catdirilma var");
-// } else {
-//   console.log("catdirilma yoxdur");
-// }
-
-let a = prompt("promptdan gelen deyer");
-if (a === "") {
-  console.log("promptun ici boshdur");
+let a = prompt("bu eraziye catdirilma varmi?");
+let b = "Baki";
+if (a.toLowerCase() == b.toLowerCase()) {
+  console.log("catdirilma var");
 } else {
-  console.log(a);
+  console.log("catdirilma yoxdur");
 }
+
+// let a = prompt("promptdan gelen deyer");
+// if (a === "") {
+//   console.log("promptun ici boshdur");
+// } else {
+//   console.log(a);
+// }
